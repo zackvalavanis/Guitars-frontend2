9987cfdbe2eaf3792ad36797b25fa5c936a5f3e2
Zack <168221270+Zval321@users.noreply.github.com>
2024-09-25 14:50:07 -0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { Header } from './Header'
+import { Footer } from './Footer'
+import { GuitarsPage } from './GuitarsPage'
+
+
+function App () { 
+  return ( 
+    <div>
+      <Header />
+      <Footer />
+      <GuitarsPage />
+    </div>
+  );
+}
+
+export default App
---
+++ src/GuitarsPage.jsx
@@ -0,0 +1,7 @@
+export function GuitarsPage () { 
+  return (
+    <main>
+      <h1>These are all your Guitars!</h1>
+    </main>
+  );
+}
